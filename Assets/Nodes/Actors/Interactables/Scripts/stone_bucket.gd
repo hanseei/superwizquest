2a19ea3c8f8c02d6810fc125d7a066d2fddb0b8f
bjarne <bjarnewilhelmhaugen@hotmail.com>
2025-08-05 14:25:12 +0200

    stone bucket now reads stone

--- Assets/Nodes/Actors/Interactables/Scripts/stone_bucket.gd
+++ Assets/Nodes/Actors/Interactables/Scripts/stone_bucket.gd
@@ -4,5 +4,12 @@ func _ready():
 	activateGroups = ["stones"]
 	
 
+func body_entered(body: Node):
+	for group in activateGroups:
+		if (body.is_in_group(group)):
+			activateTrigger()
+			return
+
 func Stone_inside():
 	print("stone in box")
+	
