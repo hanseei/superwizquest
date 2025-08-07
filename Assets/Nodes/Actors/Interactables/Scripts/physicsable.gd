c493bd8ae98bb2bad0ddc7641d5b4b36a3234100
bjarne <bjarnewilhelmhaugen@hotmail.com>
2025-08-07 11:49:17 +0200

    wind on stone now works as it should

--- Assets/Nodes/Actors/Interactables/Scripts/physicsable.gd
+++ Assets/Nodes/Actors/Interactables/Scripts/physicsable.gd
@@ -5,31 +5,30 @@ class_name Physicsable
 var wind_speed = 0
 var jump_velocity = 0
 
+var moving = false
+
+
 func _on_wind_updated(x_speed: Variant, z_speed: Variant):
 		
 	if Gamecontroller.global_direction.normalized() == Vector2.RIGHT:
 		
-		wind_speed = 2000.0
+		wind_speed = 200.0
 		jump_velocity = 0
-		print("right")
 		
 	if Gamecontroller.global_direction.normalized() ==  Vector2.LEFT:
 		
 		wind_speed = -200.0
 		jump_velocity = 0
-		print("left")
 		
 	if Gamecontroller.global_direction ==  Vector2.DOWN:
 		
 		wind_speed = 0
-		jump_velocity = 16000
-		print("down")
+		jump_velocity = 1600
 		
 	if Gamecontroller.global_direction ==  Vector2.UP:
 		
 		wind_speed = 0
-		jump_velocity = -2000
-		print("up")
+		jump_velocity = -1000
 	
 	if Gamecontroller.global_direction == Vector2.ZERO:
 		wind_speed = 0
@@ -37,14 +36,13 @@ func _on_wind_updated(x_speed: Variant, z_speed: Variant):
 		print("no direction")
 	
 	
-	if x_speed != 0:
-		print("x speed")
-		apply_central_force(Vector2(wind_speed, 0))
+	if x_speed or z_speed != 0:
+		moving = true
 		await get_tree().create_timer(5.0).timeout
-		constant_force = Vector2(0, 0)
-			
-	if z_speed !=0:
-		apply_central_force(Vector2(0, jump_velocity))
-		print("z speed")
-		await get_tree().create_timer(5.0).timeout
-		constant_force = Vector2(0, 0)
+		moving = false
+		
+
+
+func _physics_process(delta: float) -> void:
+	if moving:
+		apply_central_force(Vector2(wind_speed, jump_velocity))
