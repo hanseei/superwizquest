c772c1bd6fcc6555b1ecb5a1dcb3ff4ccd5a0560
bjarne <bjarnewilhelmhaugen@hotmail.com>
2025-08-27 11:04:26 +0200

    updated respawn to now use new respawn box. added portal to next stage.

---
+++ Assets/Nodes/Actors/Interactables/Scripts/respawn.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var respawn_used = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("player")
+	if respawn_used == false:
+		Gamecontroller.respawn_counter += 1
+		respawn_used = true
+	else:
+		print(respawn_used)
--- Assets/Nodes/Actors/Interactables/Scripts/torch.gd
+++ Assets/Nodes/Actors/Interactables/Scripts/torch.gd
@@ -1,7 +1,5 @@
 extends Interactable
 
-var respawn_used = false
-
 func _ready():
 	activateGroups = ["fires"]
 	deactivateGroups = ["waters"]
@@ -12,13 +10,7 @@ func _ready():
 #	If hit with a firebolt, check if already lit, if not, light and emit 'lit' signal.
 func lightTorch():
 		$AnimatedSprite2D.animation = "burning"
-		if is_in_group("respawn"):
-			if respawn_used == false:
-				Gamecontroller.respawn_counter += 1
-				respawn_used = true
-			else:
-				pass
-		
+
 #	If hit with water, check if already doused, if not, douse and emit 'doused' signal.
 func douseTorch():
 		$AnimatedSprite2D.animation = "default"
