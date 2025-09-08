f232b80031762eda4d9f31157925d48ea2804110
Hans Emil Eid <32166248+hanseei@users.noreply.github.com>
2025-09-08 12:17:28 +0200

    Fixed errors with stone sprite rotation

--- Assets/Nodes/Actors/Interactables/Scripts/stone.gd
+++ Assets/Nodes/Actors/Interactables/Scripts/stone.gd
@@ -6,7 +6,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$StoneAnim.global_rotation = 0
 	var stoneRot = global_rotation - int(global_rotation)
-	if global_rotation < 0.25: $StoneAnim.frame = 0
-	elif global_rotation >= 0.25 && global_rotation < 0.5: $StoneAnim.frame = 1
-	elif global_rotation >= 0.5 && global_rotation < 0.75: $StoneAnim.frame = 2
-	elif global_rotation >= 0.75 && global_rotation < 1: $StoneAnim.frame = 3
+	if stoneRot < 0: stoneRot +=1
+	if stoneRot >= 0 && stoneRot < 0.25: $StoneAnim.frame = 0
+	elif stoneRot >= 0.25 && stoneRot < 0.5: $StoneAnim.frame = 1
+	elif stoneRot >= 0.5 && stoneRot < 0.75: $StoneAnim.frame = 2
+	elif stoneRot >= 0.75 && stoneRot < 1: $StoneAnim.frame = 3
