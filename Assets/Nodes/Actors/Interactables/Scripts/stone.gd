0df27222d9bf1ab04ada5bff83daec8946e4cb46
Hans Emil Eid <32166248+hanseei@users.noreply.github.com>
2025-09-08 11:49:25 +0200

    Updated stone sprite with anim, updated maps to fit new stone size, added .ico file

--- Assets/Nodes/Actors/Interactables/Scripts/stone.gd
+++ Assets/Nodes/Actors/Interactables/Scripts/stone.gd
@@ -2,3 +2,11 @@ extends Physicsable
 
 func _ready() -> void:
 	pass
+
+func _process(delta: float) -> void:
+	$StoneAnim.global_rotation = 0
+	var stoneRot = global_rotation - int(global_rotation)
+	if global_rotation < 0.25: $StoneAnim.frame = 0
+	elif global_rotation >= 0.25 && global_rotation < 0.5: $StoneAnim.frame = 1
+	elif global_rotation >= 0.5 && global_rotation < 0.75: $StoneAnim.frame = 2
+	elif global_rotation >= 0.75 && global_rotation < 1: $StoneAnim.frame = 3
